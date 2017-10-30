203e1df3d5c629457d16a28191029da836c3c95f
AfiFatchuRahman <aphiep76r@gmail.com>
2017-10-30 23:42:48 +0700

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container container-margin">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-5 offset-3 album">
             <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+                <div class="mx-auto pt-3 pb-2" style="width: 200px;">
+                  <center><h3>Logins</h3></center>
+                </div>
 
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="d-inline-block col-md-4 control-label">E-Mail Address</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                            <div class="col-md-12">
+                                <input id="email" type="email" class="d-inline-block form-control" name="email" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -28,7 +30,7 @@
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                             <label for="password" class="col-md-4 control-label">Password</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="password" type="password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -51,7 +53,7 @@
 
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary mr-2">
                                     Login
                                 </button>
 
